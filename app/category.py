99e9ccb57bddd9c2fbbd48ef26080421da83ea51
JuanEspitia14 <juandavid.espitia@mercadolibre.com.co>
2023-04-14 08:48:42 -0500

    create class and table catagoria in database mysql

--- app/category.py
+++ app/category.py
@@ -12,14 +12,33 @@ from flask_marshmallow import Marshmallow
 
 # Instancia de FLASK mi aplicacion
 app = Flask(__name__)
-
 # Cadena de conexion
 # 'mysql+pymysql://[user]:[pwd]@localhost:3306/[base_de_datos]'
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "mysql+pymysql://root:@localhost:3306/bdpythonapi"
+] = "mysql+pymysql://root:admin1234@localhost:3306/bdpythonapi"
 # Configuracion por defecto para no alertar o warning
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.app_context().push()
+
+# Inicilizamos nuestro SQLAlchemy
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class Categoria(db.Model):
+    cat_id = db.Column(db.Integer, primary_key=True)
+    cat_nom = db.Column(db.String(100))
+    cat_desp = db.Column(db.String(100))
+
+    def __init__(self, cat_nom, cat_desp):
+        self.cat_nom = cat_nom
+        self.cat_desp = cat_desp
+
+
+# Crea las tablas
+db.create_all()
 
 
 # Mensaje de Bienvenida
