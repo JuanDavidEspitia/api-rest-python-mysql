df4b0209fb778a28a8af08b8ad873e922cc0b4a1
JuanEspitia14 <juandavid.espitia@mercadolibre.com.co>
2023-04-13 21:30:49 -0500

    create config data base in mysql

--- app/category.py
+++ app/category.py
@@ -1,12 +1,31 @@
-from flask import Flask, jsonify
+# Importar Librerias Instaladas
+# pip install flask
+# pip install flask-sqlalchemy   -----Para Conectar a una BD SQL
+# pip install flask-marshmallow  -----Definir Esquema con la BD
+# pip install marshmallow-sqlalchemy
+# pip install pymysql            ------Para Conectar a MySQL Driver MySQL
+
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
 
 # Instancia de FLASK mi aplicacion
 app = Flask(__name__)
 
+# Cadena de conexion
+# 'mysql+pymysql://[user]:[pwd]@localhost:3306/[base_de_datos]'
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql+pymysql://root:@localhost:3306/bdpythonapi"
+# Configuracion por defecto para no alertar o warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+
+# Mensaje de Bienvenida
 @app.route("/", methods=["GET"])
 def index():
-    return jsonify({"Mensaje": "Bienvenido"})
+    return jsonify({"Mensaje": "Bienvenido al curso de API Rest Python"})
 
 
 if __name__ == "__main__":
