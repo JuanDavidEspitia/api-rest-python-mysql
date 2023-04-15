a6391d22977b115c40e379d33e13177ed7e086f4
JuanEspitia14 <juandavid.espitia@mercadolibre.com.co>
2023-04-14 20:29:47 -0500

    add http methods POST, PUT, DEL for category endpoint

--- app/category.py
+++ app/category.py
@@ -27,6 +27,7 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
+# Creacion de tabla categoria
 class Categoria(db.Model):
     cat_id = db.Column(db.Integer, primary_key=True)
     cat_nom = db.Column(db.String(100))
@@ -39,6 +40,72 @@ class Categoria(db.Model):
 
 # Crea las tablas
 db.create_all()
+
+
+# Esquema
+class CategoriaSchema(ma.Schema):
+    class Meta:
+        fields = ("cat_id", "cat_nom", "cat_desp")
+
+
+# Una sola Respuesta
+categoria_schema = CategoriaSchema()
+
+# Cuando sean muchas respuestas
+categorias_schema = CategoriaSchema(many=True)
+
+
+# GET
+@app.route("/categoria", methods=["GET"])
+def get_categorias():
+    all_categorias = Categoria.query.all()
+    result = categorias_schema.dump(all_categorias)
+    return jsonify(result)
+
+
+# GET by ID
+@app.route("/categoria/<id>", methods=["GET"])
+def get_categoria_id(id):
+    categoria = Categoria.query.get(id)
+    return categoria_schema.jsonify(categoria)
+
+
+# POST
+@app.route("/categoria", methods=["POST"])
+def insert_categoria():
+    cat_nom = request.json["cat_nom"]
+    cat_desp = request.json["cat_desp"]
+    new_row = Categoria(cat_nom, cat_desp)
+
+    db.session.add(new_row)
+    db.session.commit()
+    return categoria_schema.jsonify(new_row)
+
+
+# PUT
+@app.route("/categoria/<id>", methods=["PUT"])
+def update_categoria(id):
+    actualizarcategoria = Categoria.query.get(id)
+
+    data = request.get_json(force=True)
+    cat_nom = data["cat_nom"]
+    cat_desp = data["cat_desp"]
+
+    actualizarcategoria.cat_nom = cat_nom
+    actualizarcategoria.cat_desp = cat_desp
+
+    db.session.commit()
+
+    return categoria_schema.jsonify(actualizarcategoria)
+
+
+# DELETE
+@app.route("/categoria/<id>", methods=["DELETE"])
+def delete_categoria(id):
+    eliminarcategoria = Categoria.query.get(id)
+    db.session.delete(eliminarcategoria)
+    db.session.commit()
+    return categoria_schema.jsonify(eliminarcategoria)
 
 
 # Mensaje de Bienvenida
